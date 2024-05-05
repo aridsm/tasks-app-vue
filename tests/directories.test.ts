65a645fdde3fa00d9bf176f0b7441540d5d70df8
ariane <aniemorelanie@gmail.com>
2024-05-05 10:47:54 -0300

    fix: ajustando o teste directories

--- tests/directories.test.ts
+++ tests/directories.test.ts
@@ -4,6 +4,23 @@ import { createTestingPinia } from "@pinia/testing";
 import Directories from "../components/directories.vue";
 import { useDirectoriesStore } from "../state/directories.store";
 
+function factory({data} = { data: {}}) {
+  return mount(Directories, {
+    global: {
+      plugins: [createTestingPinia({
+        initialState: {
+          DirectoriesStore: {
+            directories: [{id: 1, name: 'dir 1', description: 'desc 1', count: 0}, {id: 2, name: 'dir 2', description: 'desc 2', count: 0}]
+          }
+        }
+      })]
+    },
+    data() {
+      return data
+    }
+  })
+}
+
 describe("directories", () => {
   beforeEach(() => {
     setActivePinia(createPinia());
@@ -22,6 +39,8 @@ describe("directories", () => {
   it("saves a new directory", async () => {
     const DirectoriesComponent = mount(Directories);
 
+    expect(DirectoriesComponent.html()).toContain('Nenhum diretório adicionado')
+
     const directoryStore = useDirectoriesStore();
     const btnAddDirectory = DirectoriesComponent.find(
       '[data-type="add-directory"]'
@@ -39,38 +58,52 @@ describe("directories", () => {
       '[data-type="item-directory"]'
     );
 
-    expect(directoryStore.directories.length).toBe(1);
+    expect(directoryStore.directories).toHaveLength(1)
     expect(itemsDirectory.length).toBe(1);
+
+    expect(DirectoriesComponent.html()).not.toContain('Nenhum diretório adicionado')
+
   });
+
+  it('Select the right directory when it is clicked', async () => {
+    const wrapper = factory()
+    const directoryStoreSpy = useDirectoriesStore();
+
+    const firstDirectoryOptions = wrapper.find('[data-type="option-directory"]')
+    await firstDirectoryOptions.trigger('click.stop')
+
+    expect(wrapper.getCurrentComponent().data.form).toMatchObject(directoryStoreSpy.directories[0])
+
+    assert.isTrue(wrapper.getCurrentComponent().data.modalDirectoryOpen, 'form modal was open')
+
+    const btnSaveDirectory = wrapper.find(
+      '[data-type="save-directory"]'
+    );
+    
+    await btnSaveDirectory.trigger("click");
+
+    assert.isNotTrue(wrapper.getCurrentComponent().data.modalDirectoryOpen, 'form modal was closed')
+  })
 
   it("calls saveDirectoryHandler from directoriesStore / mocked", async () => {
 
-    const DirectoriesComponent = mount(Directories, {
-      global: {
-        plugins: [createTestingPinia()]
+    const wrapper = factory({
+      data: {
+        form: {
+          id: 0,
+          name: 'Nome da pasta',
+          description: 'Descrição da pasta'
+        }
       }
-    });
+    })
 
     const directoryStoreSpy = useDirectoriesStore();
 
-    const btnAddDirectory = DirectoriesComponent.find(
-      '[data-type="add-directory"]'
-    );
-
-    await btnAddDirectory.trigger("click");
-
-    assert.isTrue(DirectoriesComponent.vm.$data.modalDirectoryOpen, 'form modal was open')
-
-    const btnSaveDirectory = DirectoriesComponent.find(
+    const btnSaveDirectory = wrapper.find(
       '[data-type="save-directory"]'
     );
 
     await btnSaveDirectory.trigger("click");
-
-    DirectoriesComponent.vm.$data.form.name = 'Nome da pasta'
-    DirectoriesComponent.vm.$data.form.description = 'Descrição da pasta'
-
-    assert.isNotTrue(DirectoriesComponent.vm.$data.modalDirectoryOpen, 'form modal was closed')
 
     expect(directoryStoreSpy.saveDirectoryHandler).toHaveBeenCalledOnce()
 
@@ -84,71 +117,43 @@ describe("directories", () => {
   
   it("edit a directory by saveDirectoryHandler / mocked", async () => {
 
-    const DirectoriesComponent = mount(Directories, {
-      global: {
-        plugins: [createTestingPinia({
-          initialState: {
-            DirectoriesStore: {
-              directories: [{id: 1, name: 'dir 1', description: 'desc 1', count: 0}, {id: 2, name: 'dir 2', description: 'desc 2', count: 0}]
-            }
-          }
-        })]
-      }
-    });
-
+    const DirectoriesComponent = factory()
     const directoryStoreSpy = useDirectoriesStore();
 
     const firstDirectoryOptions = DirectoriesComponent.find('[data-type="option-directory"]')
     await firstDirectoryOptions.trigger('click.stop')
 
-    expect(DirectoriesComponent.vm.$data.form).toMatchObject(directoryStoreSpy.directories[0])
-
-    assert.isTrue(DirectoriesComponent.vm.$data.modalDirectoryOpen, 'form modal was open')
+    DirectoriesComponent.setData({
+      form: {
+        name: 'Novo nome'
+      }
+    })
 
     const btnSaveDirectory = DirectoriesComponent.find(
       '[data-type="save-directory"]'
     );
+    
 
     await btnSaveDirectory.trigger("click");
-
-    DirectoriesComponent.vm.$data.form.description = 'Nova descrição'
-
-    assert.isNotTrue(DirectoriesComponent.vm.$data.modalDirectoryOpen, 'form modal was closed')
 
     expect(directoryStoreSpy.saveDirectoryHandler).toHaveBeenCalledOnce()
 
     expect(directoryStoreSpy.saveDirectoryHandler).toHaveBeenCalledWith(expect.objectContaining({
-      id: 1, name: 'dir 1', description: 'Nova descrição', count: 0
+      id: 1, name: 'Novo nome', description: 'desc 1', count: 0
     }))
+
   });
 
   it('calls deleteDirectoryHandler from directoriesStore / mocked', async () => {
-    const DirectoryComponent = mount(Directories, {
-      createSpy: vi.fn,
-      global: {
-        plugins: [createTestingPinia({
-          initialState: {
-            DirectoriesStore: {
-              directories: [{id: 1, name: 'dir 1', description: 'desc 1', count: 0}, {id: 2, name: 'dir 2', description: 'desc 2', count: 0}]
-            }
-          }
-        })]
-      }
-    })
-
+    const DirectoryComponent = factory()
     const directoriesStore = useDirectoriesStore()
 
     const firstDirectoryOptions = DirectoryComponent.find('[data-type="option-directory"]')
     await firstDirectoryOptions.trigger('click.stop')
 
-    expect(DirectoryComponent.vm.$data.form).toMatchObject(directoriesStore.directories[0])
-    assert.isTrue(DirectoryComponent.vm.$data.modalDirectoryOpen, 'form modal was open')
-
     const deleteButton = DirectoryComponent.find('[data-type="delete-directory"]')
 
     await deleteButton.trigger('click')
-
-    assert.isNotTrue(DirectoryComponent.vm.$data.modalDirectoryOpen, 'form modal was closed')
 
     expect(directoriesStore.deleteDirectoryHandler).toHaveBeenCalledOnce()
     expect(directoriesStore.deleteDirectoryHandler).toHaveBeenCalledWith(1)
