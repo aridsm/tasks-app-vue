d1c3e533bd55cd17ad78ab93a8b2a2bb2007b868
ariane <aniemorelanie@gmail.com>
2024-07-06 15:04:10 -0300

    fix: ajustando count

--- components/nav-menu-item.vue
+++ components/nav-menu-item.vue
@@ -2,17 +2,21 @@
   <div class="relative ml-auto">
     <span>{{ count }}</span>
     <Transition>
-    <div
-      v-if="showCount"
-      class="absolute -right-3 bottom-6 px-2 py-1 rounded-md font-semibold text-sm"
-      :class="{
-        'bg-green-400': countAddedOne,
-        'bg-rose-400': countRemovedOne,
-      }"
-    >
-      <icon :icon="countAddedOne ? 'fa-solid fa-plus' : 'fa-solid fa-minus'" class="text-xs" /> 1
-    </div>
-</Transition>
+      <div
+        v-if="showCount"
+        class="absolute -right-3 bottom-6 px-2 py-1 rounded-md font-semibold text-sm"
+        :class="{
+          'bg-green-400': countAddedOne,
+          'bg-rose-400': countRemovedOne,
+        }"
+      >
+        <icon
+          :icon="countAddedOne ? 'fa-solid fa-plus' : 'fa-solid fa-minus'"
+          class="text-xs"
+        />
+        1
+      </div>
+    </Transition>
   </div>
 </template>
 
@@ -25,7 +29,7 @@ export default defineComponent({
     return {
       showCount: false,
       countAddedOne: false,
-      countRemovedOne: false
+      countRemovedOne: false,
     };
   },
   watch: {
@@ -33,11 +37,13 @@ export default defineComponent({
       this.countAddedOne = newValue - oldValue === 1;
       this.countRemovedOne = newValue - oldValue === -1;
 
-      this.showCount = true;
+      if (this.countAddedOne || this.countRemovedOne) {
+        this.showCount = true;
 
-      setTimeout(() => {
-        this.showCount = false;
-      }, 2000);
+        setTimeout(() => {
+          this.showCount = false;
+        }, 2000);
+      }
     },
   },
 });
@@ -46,12 +52,12 @@ export default defineComponent({
 <style scoped>
 .v-enter-active,
 .v-leave-active {
-  transition: all .4s ease;
+  transition: all 0.4s ease;
 }
 
 .v-enter-from,
 .v-leave-to {
-    bottom: -5px;
-    opacity: 0
+  bottom: -5px;
+  opacity: 0;
 }
 </style>
